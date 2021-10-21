2c0c35654a10b69fde1b32850fa0ea86ee2e4c1d
Ahmed Nabil <an0088039@gmail.com>
2021-10-21 18:43:06 +0200

    my second commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  // second commit
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
